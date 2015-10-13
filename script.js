d94282e3594cb0fe249f96227612dd55b93e8f8d
Kaleb <superjojo55@gmail.com>
2015-10-13 22:31:47 +0000

    Added the slowdraw sequence, press enter to do that

--- script.js
+++ script.js
@@ -1,55 +1,66 @@
 /*global math*/
+var DrawingFunction = false;
 $(function() {
-$("#textinput").on('change keyup paste', function() {
-    var canvas = $("#screen")[0]
-    if(canvas.getContext) {
-   		var ctx = canvas.getContext("2d")
-        ctx.clearRect(0, 0, canvas.width, canvas.height) // Under normal conditions this is how the graph will be cleared
-        canvas.width = canvas.width // In jsfiddle this seems to be the only way to clear the canvas
-        try {
-            var code = math.compile($("#textinput").val())
-            var obj = new Object()
-            obj.x = 0
-            ctx.moveTo(0, canvas.height - code.eval(obj))
-    	    for(var x = 0; x <= canvas.width; x++) {
-                obj.x = x
-             	ctx.lineTo(x, canvas.height - code.eval(obj))   
-            }
-            ctx.strokeStyle = "gray"
-    	    ctx.stroke()
-            ctx.closePath()
-            $("#error").text("  ")
-        } catch (error) {
-            $("#error").text("Invalid Equation!")
-            console.log(error)
+    $("#textinput").keypress(function (e) {
+        console.log("Oi, a thing happened!")
+        if(e.keyCode == 13) {
+            DrawingFunction = true
+            SlowGraph(math.compile($("#textinput").val()), $("#screen")[0].getContext("2d"), $("#screen")[0].width, $("#screen")[0].height)
         }
-    }
-})
-    $("#textinput").keypress(function () {
-        if($(this).keyCode == 13) {
-            SlowGraph(math.compile($("#textinput").val()), $("#screen")[0].getContext("2d"), $("#screen")[0].width)
+    })
+    $("#textinput").on('change keyup paste', function(e) {
+        var canvas = $("#screen")[0]
+        if(canvas.getContext && !DrawingFunction) {
+   	    	var ctx = canvas.getContext("2d")
+            ctx.clearRect(0, 0, canvas.width, canvas.height) // Under normal conditions this is how the graph will be cleared
+            try {
+                var code = math.compile($("#textinput").val())
+                var obj = new Object()
+                obj.x = 0
+                ctx.beginPath()
+                ctx.strokeStyle = "lightgray"
+                ctx.moveTo(0, canvas.height - code.eval(obj))
+    	        for(var x = 0; x <= canvas.width; x++) {
+                    obj.x = x
+             	    ctx.lineTo(x, canvas.height - code.eval(obj))   
+                }
+                ctx.closePath()
+                ctx.stroke()
+                $("#error").text("  ")
+            } catch (error) {
+                $("#error").text("Invalid Equation!")
+                console.log(error)
+            }
         }
     })
 })
-function SlowGraph(code, ctx, width, x) {
+function SlowGraph(code, ctx, width, height, x) {
     if(typeof x === 'undefined') x = 0;
-    if(x != 0) {
+    $("#textinput").html("")
+    if(x > 0) {
+        ctx.beginPath()
+        ctx.strokeStyle = "black"
         var obj = new Object()
         obj.x = x - 1
-        ctx.moveTo(x - 1, code.eval(obj))
+        ctx.moveTo(x - 1, height - code.eval(obj))
         obj.x++
-        ctx.lineTo(x, code.eval(obj))
-        ctx.stroke()
+        ctx.lineTo(x, height - code.eval(obj))
         ctx.closePath()
+        ctx.stroke()
     } else {
-        ctx.moveTo(0, code.eval({x:0}))
-        ctx.lineTo(1, code.eval({x:1}))
-        ctx.stroke()
+        ctx.beginPath()
+        ctx.strokeStyle = "black"
+        ctx.moveTo(0, height - code.eval({x:0}))
+        ctx.lineTo(1, height - code.eval({x:1}))
         ctx.closePath()
+        ctx.stroke()
     }
     if(x <= width) {
         setTimeout(function() {
-            SlowGraph(code, ctx, width, x)
-        }, 16)
+            SlowGraph(code, ctx, width, height, x+1)
+        }, 3)
+    } else {
+        console.log("Finished!")
+        DrawingFunction = false
     }
 }
