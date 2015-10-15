f701875338024a7cdd9de1f9a52a011df24f3d73
Jadon Clegg <jadon.clegg@gmail.com>
2015-10-15 16:22:10 -0600

    Added some comments

--- script.js
+++ script.js
@@ -7,19 +7,19 @@ $("#textinput").on('change keyup paste', function() {
         ctx.clearRect(0, 0, canvas.width, canvas.height) // Under normal conditions this is how the graph will be cleared
         canvas.width = canvas.width // In jsfiddle this seems to be the only way to clear the canvas
         try {
-		var myDiv = document.getElementById("values");
-		var myString = ""
+		var myDiv = document.getElementById("values");//set up a div to push values out to for debugging
+		var myString = "" // string containing values that will be pushed to the div
             var code = math.compile($("#textinput").val())
             var obj = new Object()
             obj.x = 0
             ctx.moveTo(0, canvas.height - code.eval(obj))
     	    for(var x = 0; x <= canvas.width; x++) {
-                var shouldBreak = false;
+                var shouldBreak = false; //bool to break out of the loop when the edge is hit
 		obj.x = x
              	var myNum = (code.eval(obj));
-		if(myNum < -1){
+		if(myNum < -.1){//When the number gets enourmous (+-100000000000000ish) it doesn't graph at all, and you never see negative vals anyways, so set it to -1 if it is under -.1
 			myNum = -1;	
-			shouldBreak = true;
+			shouldBreak = true;//Don't break yet because it hasn't graphed the final point...
 		}
 		else if(myNum > canvas.height + .1){
 			myNum = canvas.height + 1;
@@ -28,7 +28,7 @@ $("#textinput").on('change keyup paste', function() {
 myString += "<br />(" + x + ", " + myNum + ")";
 		ctx.lineTo(x, canvas.height - myNum)   
 		if(shouldBreak){
-			break;
+			break; //break if it went out of bounds
 		}
             }
 		myDiv.innerHTML = myString;
