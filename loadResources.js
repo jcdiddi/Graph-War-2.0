2a39e44c5ac34e2a4483e2b37dd42092d015f334
Jadon Clegg <jadon.clegg@gmail.com>
2015-10-20 16:07:55 -0600

    Fixed nitpicky class and formatting issues :)

--- loadResources.js
+++ loadResources.js
@@ -1,5 +1,5 @@
 $(function(){
 	$("#homeMain").load("resources/navBar.html"); 
-	$("#footer").load("resources/footer.html");
-	$("#LOGO").load("resources/logo.html");
+	$(".footer").load("resources/footer.html");
+	$("#logo").load("resources/logo.html");
 });
