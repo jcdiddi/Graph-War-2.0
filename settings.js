2ebdb46128b7b7b4d08673ec0f690e6ef9d85c56
amygt <amygabriellathomas@gmail.com>
2015-10-22 21:19:18 +0000

    Added javascript to menu strip items# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.# Explicit paths specified without -i nor -o; assuming --only paths...# On branch initial-graph# Your branch is ahead of 'origin/initial-graph' by 1 commit.#   (use "git push" to publish your local commits)## Changes to be committed:#	modified:   game.html#	modified:   settings.js#

--- settings.js
+++ settings.js
@@ -1,3 +1,15 @@
 function launchSettings() {
     alert("Good Job! Have a cookie.");
 }
+function quit() {
+    var q = confirm("Press OK to quit, press cancel to continue playing.");
+    if (q == true) {
+        alert("You quit the game.");
+    }
+}
+function help() {
+    alert("This will launch a help window.")
+}
+function functions() {
+    alert("Please select the function you want to insert:\nabs()\nlog()\netc...");
+}
